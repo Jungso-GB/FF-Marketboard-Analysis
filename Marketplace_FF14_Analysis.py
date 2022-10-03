e3a2470c1c05076381619520b27b76d9489e76c6
ARK-BLKCN <48032984+sernoz@users.noreply.github.com>
2022-10-03 07:58:10 +0200

    Add files via upload

--- Marketplace_FF14_Analysis.py
+++ Marketplace_FF14_Analysis.py
@@ -1,2 +1,11 @@
 
-print("Hello")
+import pip._vendor.requests #Faire des requetes HTTP
+worlds = [39,71,80,83,85,97,400,401,33,36,42,56,66,67,402,403] #Les mondes de Chaos jusqu'à 401 compris, puis Light.
+itemmarketable = pip._vendor.requests.get("https://universalis.app/api/v2/marketable")
+
+
+for item in itemmarketable: #Pour chaque item markettable
+	for world in worlds:#On va dans chaque monde
+
+	history = pip._vendor.requests.get("https://universalis.app/api/v2/history/" +  + "2")
+	print(history.json())
