c9320903a30c50aea07e144649fb1cacf02fdfc9
Sernoz <bricegregoirebureaupc@gmail.com>
2022-10-03 22:16:18 +0200

    Changes to be committed: 	modified:   Marketplace_FF14_Analysis.py
    Ajout des variables INPUT
    Mise en place du système pour traiter un prix minimum
    Mise en place d'un système pour check le prix dans chaque monde d'un item
    Mise en place d'une sélection d'item par prix, monde en fonction du monde d'origine (qui est une variable INPUT)

--- Marketplace_FF14_Analysis.py
+++ Marketplace_FF14_Analysis.py
@@ -1,21 +1,84 @@
+# IMPORTS
 import json
+import time
 import sys
 import pip._vendor.requests #Faire des requetes HTTP
-worlds = [39,71,80,83,85,97,400,401,33,36,42,56,66,67,402,403] #Les mondes de Chaos jusqu'à 401 compris, puis Light.
-itemmarketable = pip._vendor.requests.get("https://universalis.app/api/v2/marketable").json()
+#COUNTER
+start = time.time()
+
+# WORLDS
+worldsList = [39,71,80,83,85,97,400,401,33,36,42,56,66,67,402,403]
+# CHAOS:
+# 39: Omega
+# 71: Moogle
+# 80: Cerberus
+# 83: Louisoix
+# 85: Spriggan
+# 97: Ragnarok
+# 400: Sagittarius
+# 401: Phantom
+
+# LIGHT:
+# 33: Twintania
+# 36: Lich
+# 42: Zordiak
+# 56: Phoenix
+# 66: Odin
+# 67: Shiva
+# 402: Alpha
+# 403: Raiden
+worldsName = ["Omega", "Moogle", "Cerberus", "Louisoix", "Spriggan", "Ragnarok", "Sagittarius", "Phantom", "Twintania", "Lich", "Zordiak", "Phoenix", "Odin", "Shiva", "Alpha", "Raiden"]
+
 universalisAPI = "https://universalis.app/api/v2/"
-encoding = 'utf-8'
+itemmarketable = pip._vendor.requests.get(universalisAPI + "marketable").json()
 
+# INPUT VARIABLES
+usWorld = 97 #(Ragnarok)
+coefMargin = 1.6 #(Coeff de marge souhaité)
+minimumSellPrice = 30000
 
+# PROCESSUS
+#Retirer notre monde, des mondes à analyser
+#worldsList.pop(usWorld)
+
+print("Objets interressant:")
 for item in itemmarketable: #Pour chaque item markettable
-	for world in worlds:#On va dans chaque monde
-		tempItemData = pip._vendor.requests.get(universalisAPI + str(world) + "/" + str(item)).json()
-		
-		
-		print(tempItemData)
-		exit()
+	#Je crée le dictionnaire qui va stocker tous les prix de l'item
+	pricePerWorld = {}
+	print(item)
+ 
+	#Je récupère le prix du serveur souhaité
+	serverItemData = pip._vendor.requests.get(universalisAPI + str(usWorld) + "/" + str(item)).json() #Je récupère l'historique d'achat de l'item dans le monde 
+	try:
+		goalPrice = serverItemData['recentHistory'][0]["pricePerUnit"] / coefMargin #Je détermine mon prix objectif
+	except IndexError:
+		goalPrice = 'null'
+		continue
+	if goalPrice * coefMargin <= minimumSellPrice: #Si le prix de vente n'est pas au minimum celui souhaité, on passe à l'autre item
+		continue
 
+	#On va dans chaque monde
+	for world in worldsList:
+		tempItemData = pip._vendor.requests.get(universalisAPI + str(world) + "/" + str(item)).json() #Je récupère l'historique d'achat de l'item dans le monde
+		try:
+			price = tempItemData['recentHistory'][0]["pricePerUnit"] #Prix de la dernière vente
+		except IndexError:
+			price = 'null'
+			continue
 
+		#Je la stocke dans un dictionnaire, où chaque prix de chaque monde sera indiqué.
+		pricePerWorld[world] = price
+  
+	
+ #Pour chaque serveur, et donc chaque prix
+	for world, price in pricePerWorld.items():
+		print("Vérification de valeur sur les mondes..")
+		if (price <= goalPrice): #Si on a bien le coeff de marg
+			print("ID: " + str(item) + " Valeur: " + str(price) + "g Monde: " + str(world) + " Valeur dans notre monde:" + str(goalPrice * coefMargin))
 
 	#history = pip._vendor.requests.get("https://universalis.app/api/v2/history/" +  + "2")
 	#print(history.json())
+ 
+ #End of the script
+end = time.time()
+print(end - start)
