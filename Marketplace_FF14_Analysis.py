3115f795496c58d42dc4ea86d1e55ed8fc4e0ae9
sernoz <48032984+sernoz@users.noreply.github.com>
2022-10-05 10:42:11 +0200

    ItemID et affichage d'item fonctionnel

--- Marketplace_FF14_Analysis.py
+++ Marketplace_FF14_Analysis.py
@@ -53,31 +53,31 @@ for item in itemMarketable: #Pour chaque item markettable
 	serverItemData = pip._vendor.requests.get(universalisAPI + str(usWorld) + "/" + str(item)).json() #Je récupère l'historique d'achat de l'item dans le monde 
 	try:
 		goalPrice = serverItemData['recentHistory'][0]["pricePerUnit"] / coefMargin #Je détermine mon prix objectif
-	except IndexError:
+	except IndexError: #Si l'item n'a jamais eu de prix défini
 		goalPrice = 'null'
-		continue
-	if goalPrice * coefMargin <= minimumSellPrice: #Si le prix de vente n'est pas au minimum celui souhaité, on passe à l'autre item
-		continue
+		continue #On passe à l'item suivant
+	if goalPrice * coefMargin <= minimumSellPrice: #Si le prix de vente n'est pas au minimum celui souhaité
+		continue #On passe à l'item suivant
 
 	#On va dans chaque monde
 	for world in worldsList:
 		tempItemData = pip._vendor.requests.get(universalisAPI + str(world) + "/" + str(item)).json() #Je récupère l'historique d'achat de l'item dans le monde
 		try:
 			price = tempItemData['recentHistory'][0]["pricePerUnit"] #Prix de la dernière vente
-		except IndexError:
+		except IndexError: #Si y'a jamais eu de vente, et donc le prix de la dernière vente n'existe pas.
 			price = 'null'
 			continue
 
 		#Je la stocke dans un dictionnaire, où chaque prix de chaque monde sera indiqué.
 		pricePerWorld[world] = price
-  
+
 	
  #Pour chaque serveur, et donc chaque prix
 	print("Vérification de valeur sur les mondes..")
 	for world, price in pricePerWorld.items():
 		if (price <= goalPrice): #Si on a bien le coeff de marg
 			print(str(item))
-			itemName = itemsID[item]["fr"][0]
+			itemName = itemsID[str(item)]["fr"]
 			print("Item: " + str(itemName) + " Valeur: " + str(price) + "g Monde: " + str(world) + " Valeur dans notre monde:" + str(goalPrice * coefMargin))
 
 	#history = pip._vendor.requests.get("https://universalis.app/api/v2/history/" +  + "2")
