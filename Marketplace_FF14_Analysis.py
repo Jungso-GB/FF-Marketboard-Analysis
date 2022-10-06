2d5b88dd8aab2b66dfccf493eb790a74b356d68f
sernoz <48032984+sernoz@users.noreply.github.com>
2022-10-06 09:44:04 +0200

    Add items folder with itemID.json with usWorld pricing and the world's price interresting

--- Marketplace_FF14_Analysis.py
+++ Marketplace_FF14_Analysis.py
@@ -2,6 +2,8 @@
 import json
 import time
 import calendar
+import os
+import shutil
 import sys
 import pip._vendor.requests #Faire des requetes HTTP
 from datetime import datetime, timedelta
@@ -39,7 +41,7 @@ itemMarketable = pip._vendor.requests.get(universalisAPI + "marketable").json()
 
 # INPUT VARIABLES
 usWorld = 97 #(Ragnarok)
-coefMargin = 2 #(Coeff de marge souhaité)
+coefMargin = 1.4 #(Coeff de marge souhaité)
 minimumSellPrice = 30000
 dayDelta = 3
 
@@ -48,11 +50,22 @@ dayDelta = 3
 #worldsList.pop(usWorld)
 
 
+#Si le dossier 'items' existe pas, on le créer. Puis on va dedans.
+filepath = './items/'
+if os.path.exists(filepath) == False:
+	os.makedirs(filepath, mode = 511, exist_ok= False)
+else:
+	shutil.rmtree(filepath)
+	os.makedirs(filepath, mode = 511, exist_ok= False)
+
+os.chdir(filepath)
+
 print("Objets interressants:")
 for item in itemMarketable: #Pour chaque item markettable
 	#Je crée le dictionnaire qui va stocker tous les prix de l'item
 	pricePerWorld = {}
-	print(item)
+	priceGoalSuccess = {}
+
 	#Je récupère l'historique d'achat de l'item dans le monde 
 	serverItemData = pip._vendor.requests.get(universalisAPI + str(usWorld) + "/" + str(item)).json()
 	
@@ -81,6 +94,10 @@ for item in itemMarketable: #Pour chaque item markettable
 	if goalPrice * coefMargin <= minimumSellPrice: #Si le prix de vente n'est pas au minimum celui souhaité
 		continue #On passe à l'item suivant
 
+
+	#Je stocke le prix de notre monde au tout début de l'itemID.json
+	priceGoalSuccess[usWorld] = goalPrice * coefMargin 
+
 	#On va dans chaque monde
 	for world in worldsList:
 		tempItemData = pip._vendor.requests.get(universalisAPI + str(world) + "/" + str(item)).json() #Je récupère l'historique d'achat de l'item dans le monde
@@ -99,10 +116,15 @@ for item in itemMarketable: #Pour chaque item markettable
 		if (price <= goalPrice): #Si on a bien le coeff de marg
 			print(str(item))
 			itemName = itemsID[str(item)]["fr"]
-			print("Item: " + str(itemName) + " Valeur: " + str(price) + "g Monde: " + str(world) + " Valeur dans notre monde:" + str(goalPrice * coefMargin))
+			#Stocker dans un JSON
+			priceGoalSuccess[world] = price
+			
+			#print("Item: " + str(itemName) + " Valeur: " + str(price) + "g Monde: " + str(world) + " Valeur dans notre monde:" + str(goalPrice * coefMargin))
 
-	#history = pip._vendor.requests.get("https://universalis.app/api/v2/history/" +  + "2")
-	#print(history.json())
+#Je crée le fichier itemID.json où je stocke les prix interressants avec leur mondes
+	with open(str(item) +'.json', 'a') as file:
+		file.write(json.dumps(priceGoalSuccess))
+	exit()
  
  #End of the script
 end = time.time()
